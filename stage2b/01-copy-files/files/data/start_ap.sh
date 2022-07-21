b8473a281d9035d0761c8a5ab98739bcffd66721
David Weese <tre@gmx.de>
2022-07-21 09:27:33 +0200

    more packages

--- stage2b/01-copy-files/files/data/start_ap.sh
+++ stage2b/01-copy-files/files/data/start_ap.sh
@@ -4,7 +4,8 @@ source /data/channel.conf
 source /data/ssid.conf
 source /data/key.conf
 
-/sbin/ifdown $DEV
+#/sbin/ifdown $DEV
+/usr/sbin/ip link set $DEV down 
 /usr/bin/create_ap --no-virt -c $CHANNEL -n $DEV $SSID $KEY
 
 
